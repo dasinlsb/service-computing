133117f68127cda1edc8c135308457950b405931
dasinlsb <399359237@qq.com>
2019-12-09 16:44:17 +0800

    Fix simple-web/build.bash

--- simple-web/build.bash
+++ simple-web/build.bash
@@ -1,7 +1,11 @@
+set -ex
+
+DOCKER_COMPOSE_VERSION=1.25.0
+
 sudo rm /usr/local/bin/docker-compose
 curl -L https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > docker-compose
-chmod +x docker-compose
+chmod +x ./docker-compose
 ./docker-compose --version
 sudo mv docker-compose /usr/local/bin
-sudo docker-compose pull
-sudo docker-compose build
+sudo docker-compose -p simple-web pull
+sudo docker-compose -p simple-web build
